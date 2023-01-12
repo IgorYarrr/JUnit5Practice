4867f4f6cf70620b472677ce48518abd56c77913
IgorYarrr <igoryarrroshevich@gmail.com>
2023-01-12 12:07:12 +0300

    Homework 4 - Done

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,13 +1,58 @@
 package org.example;
 
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 public class AppTest 
 {
+    @BeforeAll
+    static void beforeAll(){
+        System.out.println("Запуск BeforeAll");
+    }
+
+    @BeforeEach
+    public void beforeEach(){
+        System.out.println("Запуск BeforeEach");
+    }
+
     @Test
-    public void shouldAnswerWithTrue()
+    @DisplayName("Тест 1")
+    @Tag("Special")
+    public void test1()
     {
-        assertTrue( true );
+        System.out.println("Запуск теста 1");
+    }
+    @Test
+    @DisplayName("Тест 2")
+    @Disabled("Починить тест!\n")
+    public void test2()
+    {
+        System.out.println("Запуск теста 2");
+    }
+    @Test
+    @DisplayName("Тест 3")
+    public void test3()
+    {
+        System.out.println("Запуск теста 3");
+        String str1 = "sample text";
+        String str2 = "sample text";
+        Assertions.assertEquals(str1, str2);
+        Assertions.assertNull(str1);
+    }
+    @Test
+    @DisplayName("Fail Test")
+    public void failTest(){
+        Assertions.fail("Ошибка!");
+    }
+
+    @AfterEach
+    public void AfterEach(){
+        System.out.println("Запуск AfterEach");
+    }
+
+    @AfterAll
+    static void AfterAll(){
+        System.out.println("Запуск AfterAll");
     }
 }
